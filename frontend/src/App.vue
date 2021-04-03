1ba1189ff906026065a26ddb4a7f5cfeafb71911
Luis Cabrera Benito <parzibyte@gmail.com>
2021-04-02 22:56:09 -0600

    Cambiando frontend de Angular a Vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <section>
+          <h1 class="is-size-1">Probando Buefy</h1>
+          <button class="button is-medium" @click="mostrarToast">
+            Mostrar un toast
+          </button>
+        </section>
+        <Vehiculos></Vehiculos>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vehiculos from "./components/Vehiculos";
+export default {
+  name: "app",
+  components: { Vehiculos },
+  methods: {
+    mostrarToast() {
+      this.$buefy.toast.open("¡Hola, Buefy! Parzibyte.me");
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/Vehiculos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <b-field label="Placas">
+        <b-input placeholder="Placas"></b-input>
+      </b-field>
+      <b-field label="Descripción del vehículo">
+        <b-input placeholder="Descripción del vehículo"></b-input>
+      </b-field>
+
+      <b-field>
+        <b-switch v-model="usarHoraActual"
+          >Usar hora actual ({{ horaActual }})</b-switch
+        >
+      </b-field>
+
+      <b-field label="Select datetime" v-show="!usarHoraActual">
+        {{ fecha instanceof Date }}
+        <b-datetimepicker
+          v-model="fecha"
+          rounded
+          placeholder="Click to select..."
+          icon="calendar-today"
+          locale="es-MX"
+          :timepicker="{ enableSeconds: true, hourFormat: '12' }"
+          horizontal-time-picker
+        >
+        </b-datetimepicker>
+      </b-field>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    fecha: null,
+    horaActual: "Soy la hora actual",
+    usarHoraActual: true,
+  }),
+};
+</script>
